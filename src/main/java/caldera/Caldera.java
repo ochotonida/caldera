a8e990dd3a9621bf40f3731ccd43aed71ce2ce37
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-06-09 22:04:57 +0200

    Initial commit

---
+++ src/main/java/caldera/Caldera.java
@@ -0,0 +1,13 @@
+package caldera;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Caldera.MODID)
+public class Caldera {
+
+    public static final String MODID = "caldera";
+
+    public Caldera() {
+
+    }
+}
